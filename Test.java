c28a144078b9e6202e745583f9e2a3128f8d27e8
SagarBahule <108591490+SagarBahule@users.noreply.github.com>
2022-07-03 01:04:44 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,3 +1,11 @@
+public class A{
+public void Rent()
+{
+System.out.println("Flat rent=15000");
+System.out.println("Hostel rent=3000");
+System.out.println("1 RK rent=7000");
+}
+}
 public class Test
 {
 public static void main(String args[])
